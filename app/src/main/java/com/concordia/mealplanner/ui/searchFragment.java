c4626dd5f1077348ad5ef5b1924998f264318d0f
harrisonahn <47039637+harrisonahn@users.noreply.github.com>
2021-04-23 12:39:47 -0400

    Fixed Search UI, added Search Bar Dialog

--- app/src/main/java/com/concordia/mealplanner/ui/searchFragment.java
+++ app/src/main/java/com/concordia/mealplanner/ui/searchFragment.java
@@ -1,8 +1,13 @@
 package com.concordia.mealplanner.ui;
 
+import android.app.Activity;
+import android.app.AlertDialog;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -10,15 +15,27 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.SearchView;
+import android.widget.TextView;
 
 import com.concordia.mealplanner.Adapter.HorizontalRecyclerViewAdapter;
 import com.concordia.mealplanner.Adapter.VerticalRecyclerViewAdapter;
 import com.concordia.mealplanner.R;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.google.android.material.appbar.AppBarLayout;
 
 
 public class searchFragment extends Fragment {
+    TextView favMealsTextView;
+    TextView availableIngTextView;
+    ImageView searchBar;
 
-
+    Activity context;
 
     //vertical view
 
@@ -41,16 +58,25 @@ public class searchFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        context = getActivity();
 
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_search, container, false);
+
+        searchBar = view.findViewById(R.id.searchBar);
+
+        searchBar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showSearchBarDialog();
+            }
+        });
+
 
         //vertical recycler view
         verticalRecyclerView = view.findViewById(R.id.recyclerview1);
@@ -73,9 +99,9 @@ public class searchFragment extends Fragment {
 
         //get reference
         horizontalRecyclerView = view.findViewById(R.id.recyclerview2);
-        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(view.getContext());
-        linearLayoutManager.setOrientation(LinearLayoutManager.HORIZONTAL); // set Horizontal Orientation
-        horizontalRecyclerView.setLayoutManager(linearLayoutManager); // set LayoutManager to RecyclerView
+        GridLayoutManager gridLayoutManager2 = new GridLayoutManager(view.getContext(),1);
+        gridLayoutManager2.setOrientation(GridLayoutManager.HORIZONTAL); // set Horizontal Orientation
+        horizontalRecyclerView.setLayoutManager(gridLayoutManager2); // set LayoutManager to RecyclerView
 
         //assign array items
         fridgeRecipeNames = getResources().getStringArray(R.array.suggested_fridge_dishes);
@@ -90,5 +116,33 @@ public class searchFragment extends Fragment {
 
 
 
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+
+    private void showSearchBarDialog() {
+        //inflate layout for this dialog
+        View view = LayoutInflater.from(getActivity()).inflate(R.layout.dialog_search_bar, null);
+
+        final EditText search_bar = view.findViewById(R.id.search_bar);
+
+        final AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        //set view to dialog
+        builder.setView(view);
+        final AlertDialog dialog = builder.create();
+        dialog.show();
+
+        search_bar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // adds new ingredient here
+
+
+                dialog.dismiss();
+            }
+        });
     }
 }
