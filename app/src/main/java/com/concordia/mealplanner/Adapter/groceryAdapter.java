979d5ed62bb8c37b3c3b75da75c89f92a9d6d095
harrisonahn <47039637+harrisonahn@users.noreply.github.com>
2021-04-22 23:04:27 -0400

    Grocery List UI

---
+++ app/src/main/java/com/concordia/mealplanner/Adapter/groceryAdapter.java
@@ -0,0 +1,54 @@
+package com.concordia.mealplanner.Adapter;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.concordia.mealplanner.R;
+import com.concordia.mealplanner.ui.groceryFragment;
+
+public class groceryAdapter extends RecyclerView.Adapter<groceryAdapter.MyViewHolder> {
+    String data[];
+    groceryFragment context;
+
+    public groceryAdapter(groceryFragment ct, String[] ingredients, int[] quantity){
+        context = ct;
+        data = ingredients;
+    }
+
+
+    @NonNull
+    @Override
+    public groceryAdapter.MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater inflater = LayoutInflater.from(parent.getContext());
+        View view = inflater.inflate(R.layout.grocery_rows, parent,false);
+        return new MyViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+        holder.ingredientText.setText(data[(position)]);
+    }
+
+    @Override
+    public int getItemCount() {
+        return data.length;
+    }
+
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+        TextView ingredientText;
+        TextView quantityText;
+        CheckBox checkBox;
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+            ingredientText = itemView.findViewById(R.id.ingredientTextView);
+        }
+    }
+}
--- app/src/main/java/com/concordia/mealplanner/ui/wasteFragment.java
+++ app/src/main/java/com/concordia/mealplanner/ui/wasteFragment.java
@@ -10,7 +10,6 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import com.concordia.mealplanner.Adapter.calendarAdapter;
 import com.concordia.mealplanner.Adapter.wasteAdapter;
 import com.concordia.mealplanner.R;
 
@@ -34,7 +33,7 @@ public class wasteFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_waste, container, false);
-        recyclerView = view.findViewById(R.id.recyclerViewWaste);
+        recyclerView = view.findViewById(R.id.recyclerViewGrocery);
         ingredients = getResources().getStringArray(R.array.ingredients_of_the_week);
         wasteAdapter wAdapter = new wasteAdapter(this, ingredients, images);
         recyclerView.setLayoutManager(new LinearLayoutManager(recyclerView.getContext()));
